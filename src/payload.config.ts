a76a2a2ffd7dcc735664d47e1042967e3685d293
Rohithpixelab <rohith.gold.9@gmail.com>
2026-04-01 20:47:37 +0530

    3rd commit

---
+++ src/payload.config.ts
@@ -0,0 +1,54 @@
+import { buildConfig } from 'payload'
+import { postgresAdapter } from '@payloadcms/db-postgres'
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+
+export default buildConfig({
+  admin: {
+    user: 'users',
+    importMap: {
+      baseDir: path.resolve(dirname),
+    },
+  },
+  collections: [
+    {
+      slug: 'users',
+      auth: true,
+      fields: [],
+    },
+    {
+      slug: 'projects',
+      fields: [
+        {
+          name: 'title',
+          type: 'text',
+          required: true,
+        },
+        {
+          name: 'img',
+          type: 'text',
+          required: true,
+        },
+        {
+          name: 'class',
+          type: 'select',
+          options: ['tall', 'wide', 'normal', 'large'],
+        },
+      ],
+    },
+  ],
+  secret: process.env.PAYLOAD_SECRET || '',
+  typescript: {
+    outputFile: path.resolve(dirname, 'payload-types.ts'),
+  },
+  db: postgresAdapter({
+    pool: {
+      connectionString: process.env.DATABASE_URI || '',
+    },
+  }),
+  editor: lexicalEditor({}),
+})
